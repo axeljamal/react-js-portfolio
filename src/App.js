570d296bf7208de88efd996e36d7e32b86258733
Axel Jamal <axeltaj@gmail.com>
2023-03-13 14:13:19 +0000

    Added components, Header, Hero, Footer, Wrapper, Container, Col, Card etc

--- src/App.js
+++ src/App.js
@@ -4,18 +4,29 @@ import Home from "./pages/Home";
 import About from "./pages/About";
 import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Wrapper from "./components/Wrapper";
 
-import { Route, Routes } from "react-router-dom";
+import { Route, Router, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
+      <Router>
+        <div>
+          <Header />
+          <Wrapper>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+          </Wrapper>
+          <Footer />
+        </div>
+      </Router>
     </>
   );
 }
