dc60793b661dc049a4608e7e588bbde853658a25
Axel Jamal <axeltaj@gmail.com>
2023-03-13 11:32:19 +0000

    created folder, components, pages

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React from "react";
 import "./index.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>This is App.js</h1>
+    </div>
+  );
 }
 
 export default App;
