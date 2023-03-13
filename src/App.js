1498756da198d3ada7b72605341085e31c3a0966
Axel Jamal <axeltaj@gmail.com>
2023-03-13 12:19:11 +0000

    Created components in pages, About, Contact, Home, Projects, import via routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React from "react";
 import "./index.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import Contact from "./pages/Contact";
+
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <h1>This is App.js</h1>
-    </div>
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </>
   );
 }
 
